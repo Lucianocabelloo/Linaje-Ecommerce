9ae3b7717b0a23090b77b51ffda86696c87be7d0
luciano cabello <lucianocabello36@gmail.com>
2022-08-24 15:06:23 -0300

    correcion titulo

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./Estilos.css";
+import "./img/home.png"
 import {FaBars, FaTimes} from "react-icons/fa";
 
 function NavBar() {
     return (
     <header className='header'>
-        <h2 className='header__logo'>Logo</h2>
+        <h2 className='header__logo'>Linaje</h2>
         <div className='listaContainer'>
             <ul className='listaContainer_ul'>
+                
                 <li className='listaContainer_ul_li'><a className='listaContainer-ul_li_a' href="/#">Inicio</a></li>
                 <li className='listaContainer_ul_li'><a className='listaContainer-ul_li_a' href="/#">Novedades</a></li>
                 <li className='listaContainer_ul_li'><a className='listaContainer-ul_li_a' href="/#">Productos</a></li>
