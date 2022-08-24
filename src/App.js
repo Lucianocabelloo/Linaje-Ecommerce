022d9eed4627928307965d08766a4654e3a0f3ce
luciano cabello <lucianocabello36@gmail.com>
2022-08-24 14:49:08 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./Components/NavBar";
+
+
 
 function App() {
+
+    let numeroDeClase = 40
+
   return (
+    <>
+    <NavBar/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        <p style={
+          {
+            color: "salmon",
+            padding: "10px"
+          }
+        }>
+          Bienvenidos a la clase {numeroDeClase}</p>
+        }
     </div>
+        </>
   );
 }
 
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Estilos.css";
+import {FaBars, FaTimes} from "react-icons/fa";
+
+function NavBar() {
+    return (
+    <header className='header'>
+        <h2 className='header__logo'>Logo</h2>
+        <div className='listaContainer'>
+            <ul className='listaContainer_ul'>
+                <li className='listaContainer_ul_li'><a className='listaContainer-ul_li_a' href="/#">Inicio</a></li>
+                <li className='listaContainer_ul_li'><a className='listaContainer-ul_li_a' href="/#">Novedades</a></li>
+                <li className='listaContainer_ul_li'><a className='listaContainer-ul_li_a' href="/#">Productos</a></li>
+                <li className='listaContainer_ul_li'><a className='listaContainer-ul_li_a' href="/#">Lugares</a></li>
+            </ul>
+        <button className='boton'>
+                <FaTimes/>
+            </button>
+        </div>
+            <button className='boton'>
+                <FaBars/>
+            </button>
+    </header>
+            )
+}
+
+export default NavBar
