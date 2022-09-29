3e576d35a0be1b3a63c58946d5be2f5f2b6f30f4
luciano cabello <lucianocabello36@gmail.com>
2022-09-29 02:09:47 -0300

    cart view final

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import "./Estilos.css"
 import {BsCartPlus} from 'react-icons/bs';
+import { useContext } from 'react';
+import { Shop } from '../Context/CartContext';
+import { useNavigate } from 'react-router-dom';
 
 const CartWidget = () => {
+
+const {cart} = useContext(Shop)
+
+ const navigate = useNavigate()
+
+ const moverme = () =>{
+  navigate("/cart")
+ }
+
   return (
+    <> 
     <div className='carritoDiv'>
-        <BsCartPlus className='carrito'/>
+        <BsCartPlus onClick={moverme} className='carrito'/>
+    <span clas className={cart.length ? '' : '0'} >                 
+        {cart.length}                
+    </span>
     </div>
+    </>
   )
 }
 
